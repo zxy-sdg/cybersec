9472894ebdb397a6e355afc51a54781f50b7a0b1
zxy-sdg <picco-grigiolino-6x@icloud.com>
2026-02-02 10:30:53 +0100

    Update leggo.py

--- leggo.py
+++ leggo.py
@@ -1,1 +1,15 @@
+def main():
+
+    nomeFile = "logfile.txt"
+    try:
+        f= open (nomeFile, "r", encoding='utf-8')
+        righe=f.readlines()
+        for riga in righe:
+            print(riga, end="")
+
+    except FileNotFoundError as e:
+        print(f"[-] errore bloccante: {str(e)}")
+
+if __name__ == "__main__":
+    main()
 
